aa0e669b01874b44d6d6f3c4e644cc6a132f7cae
Kapil Kumaria <42500918+kapilkumaria@users.noreply.github.com>
2025-11-02 11:03:11 -0700

    add main.tf providers.tf variables.tf for bith dev and prod envs

---
+++ infra/terraform/envs/dev/variables.tf
@@ -0,0 +1,28 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources in"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Environment name (dev or prod)"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnets"
+  type        = list(string)
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
---
+++ infra/terraform/envs/prod/main.tf
@@ -0,0 +1,9 @@
+module "vpc" {
+  source = "../../modules/vpc"
+
+  environment          = var.environment                 # prod
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  aws_region           = var.aws_region
+}
